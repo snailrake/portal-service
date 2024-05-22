7183e9522690a3e62f491c1d6f841a258c98a469
Viktor Dyukov <snailrake@mail.ru>
2024-05-22 16:18:41 +0400

    Initial commit

---
+++ src/test/java/ru/intership/portalservice/PortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intership.portalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
